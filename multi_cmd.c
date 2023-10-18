7d829ff6880f959576c7309bb03e3511f7282ee5
Matsadura <zidanditoo1@gmail.com>
2023-10-18 14:49:28 +0100

    Add X_M function

--- multi_cmd.c
+++ multi_cmd.c
@@ -27,3 +27,25 @@ char *_Multi(char *L)
 
 	return (NULL);
 }
+
+/**
+ * X_M - the commands to execute
+ * @d: the delimiter
+ * @L: the line that contains the commands
+ * @X: the exit code
+ * @N: the number of consecutive commands
+ * @P: the name of the program
+ * @p: the ph singly linked list
+ * @env: the copy environement
+ */
+void X_M(char *d, char *L, int *X, size_t *N, char *P, list_t *p, New_env *env)
+{
+	if (_strcmp(d, ";") == 0)
+		*X = M_c(d, L, N, P, p, env, 0);
+
+	else if (_strcmp(d, "&&") == 0)
+		*X = M_c(d, L, N, P, p, env, 1);
+
+	else if (_strcmp(d, "||") == 0)
+		*X = M_c(d, L, N, P, p, env, 2);
+}
