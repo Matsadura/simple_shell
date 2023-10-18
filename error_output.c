84ae17a88534411f341f93882bd3f992c8ccb701
Matsadura <zidanditoo1@gmail.com>
2023-10-18 14:17:15 +0100

    Add error_exec function

--- error_output.c
+++ error_output.c
@@ -61,3 +61,36 @@ void error_ret(char *err_msg, char *prg, size_t ncmd,
 	_strcat(err_msg, tmp);
 	_strcat(err_msg, "\n");
 }
+
+/**
+ * error_exec - generates an error message for
+ *	execve function if the file is not found
+ * @err_msg: the err's message string concatenated
+ * @prg: the name of the program
+ * @ncmd: the count of successive cmds executed
+ *	in the program
+ * @cmd: the executed command
+ */
+
+void error_exec(char *err_msg, char *prg, size_t ncmd,
+		char *cmd)
+{
+	unsigned int l = 0, n = ncmd;
+	char num[20], *delim = ": ", *err1 = ": not found";
+
+	while (n != 0)
+	{
+		num[l] = n % 10 + '0';
+		n /= 10, l++;
+	}
+	num[l] = '\0';
+
+	l = 0;
+	_strcpy(err_msg, prg);
+	_strcat(err_msg, delim);
+	_strcat(err_msg, num);
+	_strcat(err_msg, delim);
+	_strcat(err_msg, cmd);
+	_strcat(err_msg, err1);
+	_strcat(err_msg, "\n");
+}
