abfc43ed2e3d7d022ae12e9b5cb6dbbde79dc879
Matsadura <zidanditoo1@gmail.com>
2023-10-18 14:52:26 +0100

    Add _strcpy function

--- strings.c
+++ strings.c
@@ -45,3 +45,22 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
+/**
+ * _strcpy - copies a strings
+ * @src: the string to copy
+ * @dest: the destination
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
