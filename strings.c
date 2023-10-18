b04089fd37845f0463fd2acfc28d16029339c266
Matsadura <zidanditoo1@gmail.com>
2023-10-18 14:52:09 +0100

    Add _strlen function

--- strings.c
+++ strings.c
@@ -27,3 +27,21 @@ char *_strdup(char *str)
 	return (s);
 
 }
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
