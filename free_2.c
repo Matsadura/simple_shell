1bdf8a86484bc05a155d9594bbac901d4f6174ab
radouane abounouas <radouaneabn@gmail.com>
2023-10-18 14:31:40 +0100

    add free_m2 function

--- free_2.c
+++ free_2.c
@@ -14,3 +14,19 @@ void free_m1(list_t *PATH, New_env *env, char *_line, char *line)
 	free_grid(env->env_var);
 	free_char1(3, _line, line, env);
 }
+
+/**
+ * free_m2 - frees multiple variables
+ * @PATH: linked list of the PATH
+ * @env: copy of the environment
+ * @_line: the clean line (input)
+ * @line: the line
+ * @cmd: the command
+ */
+
+void free_m2(list_t *PATH, New_env *env, char *_line, char *line, char *cmd)
+{
+	free_list(&PATH);
+	free_grid(env->env_var);
+	free_char1(4, _line, line, env, cmd);
+}
