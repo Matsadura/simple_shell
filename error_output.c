d78b1df638a3770ae028b9ba0f223e496658a529
Matsadura <zidanditoo1@gmail.com>
2023-10-18 14:16:50 +0100

    Add error_ret function

--- error_output.c
+++ error_output.c
@@ -27,3 +27,37 @@ void error_cd(char *BUF, char *prg, size_t ncmd, char *dir)
 	_strcat(BUF, "\n");
 
 }
+
+/**
+ * error_ret - generates an error message
+ * @err_msg: the err's message string concatenated
+ * @prg: the name of the program
+ * @ncmd: the count of successive cmds executed
+ *	in the program
+ * @cmd: the executed command
+ * @tmp: temporary string
+ */
+
+void error_ret(char *err_msg, char *prg, size_t ncmd,
+		char *cmd, char *tmp)
+{
+	unsigned int l = 0, n = ncmd;
+	char num[20], *delim = ": ", *err1 = ": Illegal number: ";
+
+	while (n != 0)
+	{
+		num[l] = n % 10 + '0';
+		n /= 10, l++;
+	}
+	num[l] = '\0';
+
+	l = 0;
+	_strcpy(err_msg, prg);
+	_strcat(err_msg, delim);
+	_strcat(err_msg, num);
+	_strcat(err_msg, delim);
+	_strcat(err_msg, cmd);
+	_strcat(err_msg, err1);
+	_strcat(err_msg, tmp);
+	_strcat(err_msg, "\n");
+}
