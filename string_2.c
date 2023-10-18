ff5fad22e3fc311ea7c1fd794f81865a0b35aa7a
radouane abounouas <radouaneabn@gmail.com>
2023-10-18 14:45:20 +0100

    add int_to_str function

--- string_2.c
+++ string_2.c
@@ -120,3 +120,34 @@ char *_strchr(char *s, char c)
 		return (&s[i]);
 	return (NULL);
 }
+
+/**
+ * int_to_str - this function coverts a integer to string
+ * @n: the number that will converted
+ * Return: return the result
+ */
+
+char *int_to_str(int n)
+{
+	int tmp = n, i;
+	char *str;
+
+	if (tmp == 0)
+		return ("0");
+
+	for (i = 0; tmp; i++)
+		tmp /= 10;
+
+	str = malloc(i + 1);
+	str[i] = '\0';
+	i--;
+
+	tmp = n;
+	for (; tmp; i--)
+	{
+		str[i] = tmp % 10 + '0';
+		tmp /= 10;
+	}
+
+	return (str);
+}
