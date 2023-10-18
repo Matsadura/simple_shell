fb3d5149b5d339ec96aab7d3452f7c4a39f5def3
radouane abounouas <radouaneabn@gmail.com>
2023-10-18 14:44:58 +0100

    add strchr

--- string_2.c
+++ string_2.c
@@ -96,3 +96,27 @@ int _strncmp(char *s1, char *s2, int num)
 		return (0);
 	return (j);
 }
+
+/**
+ * _strchr - locates a chracter in a string
+ * @s: the string
+ * @c: the char
+ * Return: pointer to first occurence of char c
+ *	or NULL if it's not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	if (s[i] == c)
+		return (&s[i]);
+	return (NULL);
+}
