eb6efea6262e4f989aacefcc40977326ed7e16e6
radouane abounouas <radouaneabn@gmail.com>
2023-10-18 14:14:33 +0100

    add the function that creates the environment

--- env.c
+++ env.c
@@ -14,3 +14,30 @@ New_env *create_env(char **environ)
 	env->env_var = cp_env(environ);
 	return (env);
 }
+
+/**
+ * cp_env - copies the Environ to Env struct
+ * @environ: the global environment
+ * Return: the copy of Environ
+ */
+
+char **cp_env(char **environ)
+{
+	int i = 0;
+	char **new_env = NULL;
+
+	while (environ[i] != NULL)
+	{
+		i++;
+	}
+
+	new_env = malloc((i + 1) * sizeof(char *));
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		new_env[i] = _strdup(environ[i]);
+	}
+	new_env[i] = NULL;
+
+	return (new_env);
+}
